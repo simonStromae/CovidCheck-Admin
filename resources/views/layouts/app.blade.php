b613ac89fc8092e2440e33876774c6374f8f852d
simonStromae <simonulrich56@gmail.com>
2019-12-04 09:44:18 +0100

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,10 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>THE_LAPA - @yield('title')</title>
-    <link type="text/css" href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link type="text/css" href="{{ asset('bootstrap/css/bootstrap-responsive.min.css') }}" rel="stylesheet">
-    <link type="text/css" href="{{ asset('css/theme.css') }}" rel="stylesheet">
-    <link type="text/css" href="{{ asset('images/icons/css/font-awesome.css') }}" rel="stylesheet">
+    <link type="text/css" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link type="text/css" href="{{ asset('assets/bootstrap/css/bootstrap-responsive.min.css') }}" rel="stylesheet">
+    <link type="text/css" href="{{ asset('assets/css/theme.css') }}" rel="stylesheet">
+    <link type="text/css" href="{{ asset('assets/images/icons/css/font-awesome.css') }}" rel="stylesheet">
     <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
     @stack('css')
 </head>
@@ -29,8 +29,8 @@
 
 @include('layouts.partial._footer')
 
-<script src="{{ asset('scripts/jquery-1.9.1.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('scripts/jquery-ui-1.10.1.custom.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/scripts/jquery-1.9.1.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/scripts/jquery-ui-1.10.1.custom.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
 @stack('scripts')
 </body>
